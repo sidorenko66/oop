18dd8641ffcfb7553462e05c692a5b2ccdd9fcdd
Aleksey Sidorenko <sidorenko66@gmail.com>
2022-06-12 21:27:21 +0500

    1 and 2 tasks

--- main.py
+++ main.py
@@ -7,16 +7,34 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-	def add_courses(self, course_name):
-   		self.finished_course.append(course_name)   
+    def add_courses(self, course_name):
+        self.finished_course.append(course_name)
 
-     
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            print(f'Ошибка: {self.name} {self.surname} can not rate {lecturer.name} {lecturer.surname} in {course}')
+            return 'Ошибка'
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -24,16 +42,26 @@ class Mentor:
             else:
                 student.grades[course] = [grade]
         else:
+            print(f'Ошибка: {self.name} {self.surname} can not rate {student.name} {student.surname} in {course}')
             return 'Ошибка'
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
 
-print(best_student.grades)
+cool_lecturer = Lecturer('Adam', 'Smith')
+cool_lecturer.courses_attached += ['Python']
+
+best_student.rate_hw(cool_lecturer, 'Python', 9)
+best_student.rate_hw(cool_lecturer, 'Python', 8)
+best_student.rate_hw(cool_lecturer, 'Python', 10)
+best_student.rate_hw(cool_lecturer, 'Economy', 1)  # error
+
+print(f'best_student.grades: {best_student.grades}')
+print(f'cool_lecturer.grades: {cool_lecturer.grades}')
